229f643466ad9c97c2db3c19e41a574af1f3a645
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:04:03 +0100

    Update to schema version v4.81.0

--- outputs.tf
+++ outputs.tf
@@ -12,7 +12,7 @@ output "new_relic_monitors_account_id" {
 }
 output "new_relic_monitors_identity" {
   description = "Map of identity values across all new_relic_monitors, keyed the same as var.new_relic_monitors"
-  value       = { for k, v in azurerm_new_relic_monitor.new_relic_monitors : k => v.identity if v.identity != null && length(v.identity) > 0 }
+  value       = { for k, v in azurerm_new_relic_monitor.new_relic_monitors : k => one(v.identity) if v.identity != null && length(v.identity) > 0 }
 }
 output "new_relic_monitors_ingestion_key" {
   description = "Map of ingestion_key values across all new_relic_monitors, keyed the same as var.new_relic_monitors"
@@ -37,7 +37,7 @@ output "new_relic_monitors_organization_id" {
 }
 output "new_relic_monitors_plan" {
   description = "Map of plan values across all new_relic_monitors, keyed the same as var.new_relic_monitors"
-  value       = { for k, v in azurerm_new_relic_monitor.new_relic_monitors : k => v.plan if v.plan != null && length(v.plan) > 0 }
+  value       = { for k, v in azurerm_new_relic_monitor.new_relic_monitors : k => one(v.plan) if v.plan != null && length(v.plan) > 0 }
 }
 output "new_relic_monitors_resource_group_name" {
   description = "Map of resource_group_name values across all new_relic_monitors, keyed the same as var.new_relic_monitors"
@@ -45,7 +45,7 @@ output "new_relic_monitors_resource_group_name" {
 }
 output "new_relic_monitors_user" {
   description = "Map of user values across all new_relic_monitors, keyed the same as var.new_relic_monitors"
-  value       = { for k, v in azurerm_new_relic_monitor.new_relic_monitors : k => v.user if v.user != null && length(v.user) > 0 }
+  value       = { for k, v in azurerm_new_relic_monitor.new_relic_monitors : k => one(v.user) if v.user != null && length(v.user) > 0 }
 }
 output "new_relic_monitors_user_id" {
   description = "Map of user_id values across all new_relic_monitors, keyed the same as var.new_relic_monitors"
